6ba6bc019dd944543c27af99eceffdbcceae0752
Ivo de Liefde | Tensing <ideliefde@tensing.com>
2020-07-09 14:25:08 +0200

    Created sklearn object and template .fmx file

---
+++ sklearn_object.py
@@ -0,0 +1,72 @@
+from sklearn import linear_model, tree, svm, cluster
+from joblib import dump, load
+
+class sklearn_model():
+    def __init__(self):
+        self.model_types = ["regression","classification", "clustering"]
+        self.model_architectures = {"regression":["linear","tree","svm","sgd"], "classification": ["tree","svm","sgd"], "clustering": ["k-means"]}
+        self.metrics = {"regression":["explained_variance", "mse"], "classification": ["accuracy","precision","f1score"], "clustering": []}
+        self.model_type = None
+        self.model_architecture = None
+        self.metric = None
+        self.model = None
+
+    def available_models(self):
+        s = ""
+        for t in self.model_types:
+            s += f"Model type: {t}\n"
+            for a in self.model_architectures[t]:
+                s += f" - {a}\n"
+        return(s)
+
+    def create_model(self, model_type, model_architecture, *args, **kwargs):
+        """ 
+        Initialise an Scikit-learn model. 
+
+        Keyword arguments:
+        model_type -- the kind of output the model should deliver (classification, regression or clustering are currently implemented)
+        model_architecture -- the method the model should use (for example, linear, tree or svm)
+        *args & **kwargs -- any additional arguments are directly passed on to the scikit-learn model. 
+        """
+        self.model_type = model_type
+        self.model_architecture = model_architecture      
+
+        if model_type == "regression" and model_architecture == "linear":
+            self.model = linear_model.LinearRegression(*args, **kwargs)
+        elif model_type == "regression" and model_architecture == "tree":
+            self.model = tree.DecisionTreeRegressor(*args, **kwargs)
+        elif model_type == "regression" and model_architecture == "svm":
+            self.model = svm.SVR(*args, **kwargs)
+        elif model_type == "regression" and model_architecture == "sgd":
+            self.model = linear_model.SGDRegressor(*args, **kwargs)
+        elif model_type == "classification" and model_architecture == "tree":
+            self.model = tree.DecisionTreeClassifier(*args, **kwargs)
+        elif model_type == "classification" and model_architecture == "svm":
+            self.model = svm.SVC(*args, **kwargs)
+        elif model_type == "classification" and model_architecture == "sgd":
+            if len(kwargs.items()) > 0 or len(args) > 0:
+                self.model = linear_model.SGDClassifier(loss="hinge", penalty="l2", max_iter=5)
+            else:
+                self.model = linear_model.SGDClassifier(*args, **kwargs)
+        elif model_type == "clustering" and model_architecture == "k-means":
+            if len(kwargs.items()) > 0 or len(args) > 0:
+                self.model = cluster.KMeans(n_clusters=2, random_state=0)
+            else:
+                self.model = cluster.KMeans(*args, **kwargs)
+        else:
+            raise f"Combination of model type '{model_type}' and architecture '{model_architecture}' is invalid."
+
+    def export_model(self, path="fme_model"):
+        self.export_path = path
+        dump(self.model, f"{path}.joblib")
+    
+    def import_model(self, path="fme_model.joblib"):
+        self.import_path = path
+        self.model = load(path)
+
+if __name__ == "__main__":
+    fme_ml = sklearn_model()
+    print(fme_ml.available_models())
+    fme_ml.create_model("classification", "svm")
+    print(fme_ml.model.get_params())
+    print(fme_ml)
---
+++ sklearn_transformer.py
@@ -0,0 +1,62 @@
+﻿#============================================================================
+#
+#  Name     : sklearn_transformer.py
+#  
+#  System   : FME Custom Transformer
+#  
+#  Language : Python
+#  
+#  Purpose  : Enabling FME users to implement Scikit-learn models inside a workspace.
+# 
+#        Copyright (c) 2020, Tensing. All rights reserved.
+#
+#   Redistribution and use of this sample code in source and binary forms, with 
+#   or without modification, are permitted provided that the following 
+#   conditions are met:
+#   * Redistributions of source code must retain the above copyright notice, 
+#     this list of conditions and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright notice, 
+#     this list of conditions and the following disclaimer in the documentation 
+#     and/or other materials provided with the distribution.
+# 
+#===============================================================================
+
+import fmeobjects
+from TransformerUtil import Transformer
+from sklearn_object import sklearn_model
+import numpy as np
+
+#============================================================================
+# Class to perform the overall logic of training a machine learning model.
+class MachineLearningModelTrainer(Transformer):
+   
+   def __init__(self, instanceName, paramMap):
+      Transformer.__init__(self, instanceName, paramMap)
+      self.logger = fmeobjects.FMELogFile()
+      self.logger.logMessageString("Transformer initialized", fmeobjects.FME_INFORM)
+      self.sk = sklearn_model()
+      self.target_variable = self.paramMap()["target_variable"]
+      self.model_type = self.paramMap()["model_type"]
+      self.model_architecture = self.paramMap()["model_architecture"]
+      self.sk.create_model(self.model_type, self.model_architecture)
+      self.x = None
+      self.y = None
+
+   # Takes a feature and processes it
+   def input(self, feature):
+      self.logger.logMessageString("Feature type : "+feature.getAttribute("fme_feature_type"), fmeobjects.FME_INFORM)
+
+      # Collect data
+
+      # Send the feature on its way
+      self.pyoutput(feature)
+      self.logger.logMessageString("Feature processed", fmeobjects.FME_INFORM)
+
+   def close(self):
+      # Fit data to model
+
+      # Export model
+
+
+      self.logger.logMessageString("Transformer closed", fmeobjects.FME_INFORM)
+      del(self.logger) # Needed to avoid "Not All FME sessions were destroyed"
