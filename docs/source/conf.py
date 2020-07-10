db6c02f81d2e096f3a3229d3e664b4ef582cff6e
Ivo de Liefde | Tensing <ideliefde@tensing.com>
2020-07-10 23:57:24 +0200

    build sphinx pages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ release = '0'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.githubpages',
+    #'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
