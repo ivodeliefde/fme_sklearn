37dc8f97c9072064dd892984370f21c5f81bd3f5
Ivo de Liefde | Tensing <ideliefde@tensing.com>
2020-07-11 14:47:19 +0200

    Created install script for FME on windows

---
+++ install_transformers.bat
@@ -0,0 +1,3 @@
+xcopy /y /q Transformers\*.fmx C:\Users\%USERNAME%\Documents\FME\Transformers\ 
+xcopy /y /q Transformers\sklearn*.py C:\Users\%USERNAME%\Documents\FME\Plugins\Python\python37\
+xcopy /y /q Transformers\sklearn_object C:\Users\%USERNAME%\Documents\FME\Plugins\Python\python37\sklearn_object\
